7d242f8a32eb6e222efc8aa7fa23e6915e306092
TurtleFeeder <t.choong@yahoo.com>
2018-11-11 12:36:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, obj, arg2) {
+  return fn.call(obj, arg2)
+}
+
+function setThisWithApply(fn, obj, arr) {
+  return fn.apply(obj, arr);
+}
+
+function returnNewFunctionOf(fn, obj) {
+  return fn.bind(obj)
+}
